c0455896abe1e1a90703df8ff975725d44ee2868
C-M-007 <96769523+C-M-007@users.noreply.github.com>
2022-01-26 19:15:36 +0530

    Add files via upload

---
+++ CSS(2)output.py.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\asus-PC\Desktop\CODE\Python College\CSS(2).py.py ======
+36
+.egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language.
+10
+Pythonisacasesensitivelanguage.
+Whats your name:Chiranjeev
+Enter Your SID:21104083
+Enter your Branch:Electrical
+Enter your CGPA:10
+Hey , Chiranjeev Here!
+My SID is 21104083
+I am from Electrical department and my CGPA is 10.0
+8
+58
+50
+224
+40
+14
+0
+Enter your 1st number here:45
+Enter your 2nd number here:7
+Enter your 3rd number here:7
+The largest number is 45.0
+type: my name is cj
+Yes
+Type down the first side:78
+Type down the second side:7
+Type down the third side:77
+Yes,These sides can form a triangle.

c98ba00f96906ac9ea653962f42fea9bddcadfc3
C-M-007 <96769523+C-M-007@users.noreply.github.com>
2022-02-27 16:14:32 +0530

    Create CSS(4) Output.py

---
+++ CSS(4) Output.py
@@ -0,0 +1,48 @@
+Move Disk  1  from rod  A  to rod  B
+Move Disk  2  from rod  A  to rod  B
+Move Disk  1  from rod  C  to rod  A
+Move Disk  3  from rod  A  to rod  B
+Move Disk  1  from rod  C  to rod  A
+Move Disk  2  from rod  C  to rod  A
+Move Disk  1  from rod  B  to rod  C
+
+
+Enter the number Of rows of The Pascal's Triangle: 4
+Solving by Recursion Method...
+      1   
+    1   1   
+  1   2   1   
+1   3   3   1
+
+
+Solving by Iterative Method...
+     1
+    1 1
+   1 2 1
+  1 3 3 1
+
+
+
+Enter 2 Numbers: 2 7
+Callability Check...
+False
+False
+The Quotient Is Zero
+Filtered out numbers that are greater than 4 are : [6, 7, 5, 7, 6, 8]
+Set:  {8, 5, 6, 7}
+Immutable set: frozenset({8, 5, 6, 7})
+Hash value of the max value from the above set: 8
+
+
+File Created
+The name Of the student is Chiranjeev and SID is 21104083.
+File Destroyed!
+
+
+The Updated Salary Of Mehak Is 70000
+Record Of Viren Deleted!
+
+Enter Any Word: Chiranjeev
+Enter A New Word Using The Exact Same Letters: Veejnarihc
+Does The Word Makes Any Sense?(Y Or N) N 
+Meaningless Word , Friendship Is Fake!

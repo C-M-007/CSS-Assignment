db2df2fbd0d08f40af994a6c99e29b0f4b79ffcf
C-M-007 <96769523+C-M-007@users.noreply.github.com>
2022-03-02 12:43:06 +0530

    Update CSS(4)Output.py

--- CSS(4)Output.py
+++ CSS(4)Output.py
@@ -12,26 +12,28 @@ Solving by Recursion Method...
       1   
     1   1   
   1   2   1   
-1   3   3   1
+1   3   3   1   
 
 
+Enter number of rows for the pascal triangle:5
 Solving by Iterative Method...
-     1
-    1 1
-   1 2 1
-  1 3 3 1
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
 
 
 
-Enter 2 Numbers: 2 7
+Enter 2 Numbers: 3 6
 Callability Check...
 False
 False
 The Quotient Is Zero
-Filtered out numbers that are greater than 4 are : [6, 7, 5, 7, 6, 8]
-Set:  {8, 5, 6, 7}
-Immutable set: frozenset({8, 5, 6, 7})
-Hash value of the max value from the above set: 8
+Filtered out numbers that are greater than 4 are : [7, 8, 5, 8, 6, 9]
+Set:  {5, 6, 7, 8, 9}
+Immutable set: frozenset({5, 6, 7, 8, 9})
+Hash value of the max value from the above set: 9
 
 
 File Created
@@ -39,10 +41,10 @@ The name Of the student is Chiranjeev and SID is 21104083.
 File Destroyed!
 
 
-The Updated Salary Of Mehak Is 70000
+The Updated Salary Of Mehak Is 70000 
 Record Of Viren Deleted!
 
-Enter Any Word: Chiranjeev
-Enter A New Word Using The Exact Same Letters: Veejnarihc
-Does The Word Makes Any Sense?(Y Or N) N 
+Enter Any Word: jet
+Enter A New Word Using The Exact Same Letters: tej
+Does The Word Makes Any Sense?(Y Or N) N
 Meaningless Word , Friendship Is Fake!
